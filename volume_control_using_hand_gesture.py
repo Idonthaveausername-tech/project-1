dafb3cfb77ff6133d606fb7c0577c6c8e308e115
Vinay Pratyush <65728746+Idonthaveausername-tech@users.noreply.github.com>
2022-10-01 19:28:02 +0530

    Update volume_control_using_hand_gesture.py

--- volume_control_using_hand_gesture.py
+++ volume_control_using_hand_gesture.py
@@ -5,13 +5,6 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import numpy as np
-import cv2
-import mediapipe as mp
-from math import hypot
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-import numpy as np
  
 cap = cv2.VideoCapture(0) #Checks for camera
  
